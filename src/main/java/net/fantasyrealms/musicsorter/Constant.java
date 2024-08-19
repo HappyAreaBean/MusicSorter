14f90608866964905875ff91796780001c5e8f14
HappyAreaBean <bean@happyareabean.cc>
2024-08-19 16:01:29 +0800

    release: 1.0.2

--- src/main/java/net/fantasyrealms/musicsorter/Constant.java
+++ src/main/java/net/fantasyrealms/musicsorter/Constant.java
@@ -7,7 +7,7 @@ import java.util.List;
 @UtilityClass
 public class Constant {
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String DEFAULT_INPUT = "input";
     public static final String DEFAULT_OUTPUT = "output";
     public static final String INVALID_CHAR = "[\\\\/:*?\"<>|]";
